3f342214b4a013aab1e21c2ff7ee1d43846007de
Clem Morton <clem16@gmail.com>
2020-03-19 19:21:14 -0700

    Woops. fixed passing session to the def

--- getlibrary.cr
+++ getlibrary.cr
@@ -8,7 +8,7 @@ session = Path["~/.audible.json"].expand(home: true)
 get_library(session)
 
 # Basic library call (NOTE: limited to 99 items.)
-def get_library(config)
+def get_library(session)
   client = Audible::Client.from_json(File.read("#{session}"))
   puts client.get("/1.0/library").body
 end
